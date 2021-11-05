c95c3622f5fa034becfde4734710f2c8154f2e2e
Mohammad Zarchi <mohammad.zarchi1373@gmail.com>
2021-11-05 15:15:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ import tweepy
 
 import time
 
-cf = {
-    'key': '*****',
-    'kse': '*****',
-    'tok': '*****',
-    'tse': '*****'
-}
-
-auth = tweepy.OAuthHandler(cf['key'], cf['kse'])
-auth.set_access_token(cf['tok'], cf['tse'])
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
 
 def num(n):
     if n == 0:
@@ -41,16 +30,16 @@ def num(n):
     return result
 
 
-def data(screen_name):
+def data(tapi, screen_name):
     followers = []
     followings = []
-    for page in tweepy.Cursor(api.get_follower_ids, screen_name=screen_name).pages():
+    for page in tweepy.Cursor(tapi.get_follower_ids, screen_name=screen_name).pages():
         for c in page:
             followers.append(c)
         time.sleep(1)
     print(f"Stream {len(followers):,} followers complate!")
 
-    for page in tweepy.Cursor(api.get_friend_ids, screen_name=screen_name).pages():
+    for page in tweepy.Cursor(tapi.get_friend_ids, screen_name=screen_name).pages():
         for c in page:
             followings.append(c)
         time.sleep(1)
@@ -73,36 +62,48 @@ def data(screen_name):
     return result
 
 
-def write(ids):
+def write(tapi, ids):
     counter = 1
     for p in ids:
         try:
-            info = api.get_user(user_id=p)
+            info = tapi.get_user(user_id=p)
             link = "https://twitter.com/" + info.screen_name
             print(f"No: {num(counter)}, Tweets: {num(info.statuses_count)}, Link: {link}")
         except:
             print(f"No: {num(counter)}, Id: {p} Not found!")
         counter += 1
         time.sleep(1)
-
-
-username = input("Please enter Twitter @username: ")
-user_ids = data(username)
-print("Select your function:")
-print(f"[1] People who be followed @{username}.")
-print(f"[2] People that @{username} be followed.")
-print(f"[3] People who did not followed @{username}.")
-print(f"[4] People that @{username} did not followed.")
-print(f"[5] Exit from app.")
-run = int(input("Insert your function code? (1|2|3|4|5) : "))
-
-if run == 1:
-    write(user_ids['follower'])
-elif run == 2:
-    write(user_ids['following'])
-elif run == 3:
-    write(user_ids['ptudnf'])
-elif run == 4:
-    write(user_ids['pwdnfu'])
-elif run == 5:
     input("Press any key to exit.")
+
+
+if __name__ == '__main__':
+    cf = {
+        'key': '*****',
+        'kse': '*****',
+        'tok': '*****',
+        'tse': '*****'
+    }
+    auth = tweepy.OAuthHandler(cf['key'], cf['kse'])
+    auth.set_access_token(cf['tok'], cf['tse'])
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    username = input("Please enter Twitter @username: ")
+    user_ids = data(api, username)
+    print("Select your function:")
+    print(f"[1] People who be followed @{username}.")
+    print(f"[2] People that @{username} be followed.")
+    print(f"[3] People who did not followed @{username}.")
+    print(f"[4] People that @{username} did not followed.")
+    print(f"[5] Exit from app.")
+    run = int(input("Insert your function code? (1|2|3|4|5) : "))
+
+    if run == 1:
+        write(api, user_ids['follower'])
+    elif run == 2:
+        write(api, user_ids['following'])
+    elif run == 3:
+        write(api, user_ids['ptudnf'])
+    elif run == 4:
+        write(api, user_ids['pwdnfu'])
+    elif run == 5:
+        input("Press any key to exit.")
